b4b49f2d50e98ace4750f52dfacea28f27538f35
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-05-30 13:07:58 +0100

    Minor configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     colors : {
       'darkblue' : 'hsl(233, 26%, 24%)', 
       'limegreen' : 'hsl(136, 65%, 51%)', 
       'brightcyan' : 'hsl(192, 70%, 51%)', 
-    },
-    secondaryColors : {
       'GrayBlue' : 'hsl(233, 8%, 62%)',
       'LightGrayBlue' : 'hsl(220, 16%, 96%)',
       'LightGray' : 'hsl(0, 0%, 98%)',
       'White' : 'hsl(0, 0%, 100%)',
     },
+    
     fontFamily : {
       publicSans : ['Public Sans']
     },
